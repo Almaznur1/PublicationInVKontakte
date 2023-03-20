d547494e889cf6d0e0f05d615c5e226864b3a7c6
Almaz Nuriahmetov <aboohnifa@gmail.com>
2023-03-20 07:01:01 +0300

    completed step 9

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 
-def fetch_comics():
+def fetch_comics():  # step 1, 2
     response = requests.get('https://xkcd.com/info.0.json')
     response.raise_for_status()
     response = response.json()
@@ -16,7 +16,7 @@ def fetch_comics():
         img.write(response.content)
 
 
-def get_wall_upload_server(access_token):
+def get_wall_upload_server(access_token):  # step 8
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
     params = {
         'access_token': f'{access_token}',
@@ -28,23 +28,28 @@ def get_wall_upload_server(access_token):
     upload_url = response['upload_url']
     album_id = response['album_id']
     user_id = response['user_id']
-    print(upload_url, album_id, user_id, sep='\n')
+    return upload_url
 
 
-def save_wall_photo(access_token):
-    url = 'photos.saveWallPhoto'
+def save_wall_photo(access_token, upload_url):  # step 9
+    url = upload_url
     params = {
         'access_token': f'{access_token}',
         'v': '5.131',
         'group_id': '657767863',
         }
-    response = requests.post(url, params=params)
+    with open('comix.png', 'rb') as file:
+        url = upload_url
+        files = {
+            'photo': file,
+        }
+        response = requests.post(url, files=files)
     response.raise_for_status()
+    print(response.json())
 
 
-
-def publish_comics(access_token):
-    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+def publish_comics(access_token):  # step 7 возращает имеющиеся группы
+    url = 'https://api.vk.com/method/groups.get'
     params = {
         'access_token': f'{access_token}',
         'v': '5.131',
@@ -58,7 +63,8 @@ def main():
     load_dotenv()
     access_token = os.environ['VK_ACCESS_TOKEN']
 
-    save_wall_photo(access_token)
+    upload_url = get_wall_upload_server(access_token)
+    save_wall_photo(access_token, upload_url)
 
 
 if __name__ == '__main__':
